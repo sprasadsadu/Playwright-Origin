5b5fdbeb24e1a2c30bc06d1970a133e15399add2
Prasad <sprasad.sadu@gmail.com>
2025-10-08 22:25:27 +0530

    allure-report generation
    Signed-off-by: Prasad <sprasad.sadu@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,8 +23,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['list'],
-    ['allure-playwright'],    
+    ['line'],
+    ['allure-playwright'],
+    ['html'], 
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
